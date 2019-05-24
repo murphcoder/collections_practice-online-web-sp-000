b99962e04412dc1d77df80843e52b1ce0adbf3ef
Sean Murphy <filmmakersean@fastmail.net>
2019-05-24 19:23:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,9 +45,9 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index do |element, index|
+  array.each_with_index.collect do |element, index|
     if index != 1
-      element << s
+      element << "s"
     end
   end
 end
