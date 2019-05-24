0f25b531802f3406e89ecea2dcc80a98bec24428
Sean Murphy <filmmakersean@fastmail.net>
2019-05-24 19:26:09 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,56 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  a = array[1]
+  b = array[2]
+  array[1] = b
+  array[2] = a
+  return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each {|word| word[2] = "$"}
+end
+
+def find_a(array)
+  array.select {|word| word[0] == "a"}
+end
+
+def sum_array(array)
+  array.inject {|a, b| a + b}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index != 1
+      element << "s"
+    else
+      element = element
+    end
+  end
+end
 
